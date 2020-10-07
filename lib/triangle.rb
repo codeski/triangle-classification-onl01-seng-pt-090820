e2585222fbc5d354d9daa9039c5405bbfdb4dd54
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-10-07 18:23:01 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,7 +2,7 @@ class Triangle
   
   attr_accessor :one, :two, :three
   
-  def initialize(one:, two:, three:)
+  def initialize(one, two, three)
     @one = one 
     @two = two 
     @three = three
