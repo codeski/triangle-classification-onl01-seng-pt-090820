76b68e602f4dfa4a4f27c826bc39dd3b6c77291e
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-10-07 17:58:10 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,12 +1,16 @@
 class Triangle
   
+  attr_accessor :one, :two, :three
   
-  
-  def initialize(one, two, three)
+  def initialize(one:, two:, three:)
+    @one = one 
+    @two = two 
+    @three = three
   end
   
   def kind 
-    if 
+    if 0 > @one || @two || @three 
+      raise TriangleError
     end 
   end 
   
